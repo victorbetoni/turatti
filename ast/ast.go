09ce6c3add713af180a6eafadc8f2080d0aebba8
victorbetoni <victorbetoni@protonmail.com>
2023-09-21 15:39:29 -0300

    parser: implementing infix expressions

--- ast/ast.go
+++ ast/ast.go
@@ -119,6 +119,25 @@ func (pe *PrefixExpression) String() string {
 	return out.String()
 }
 
+type InfixExpression struct {
+	Token    token.Token
+	Left     Expression
+	Right    Expression
+	Operator string
+}
+
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+	return out.String()
+}
+
 type IntegerLiteral struct {
 	Token token.Token
 	Value int64
